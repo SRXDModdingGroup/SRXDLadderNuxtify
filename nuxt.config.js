e40a381adb66076935bb5ab1159aeac457e20861
Amy Y <ctrlalternativedelete@gmail.com>
2021-07-01 16:22:48 +1000

    Revert "changed default port"
    This reverts commit 6806e53737713bedde7eee45e63331cef6dd3455.

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,7 @@ export default {
   target: 'static',
 
   server: {
-    host: '0.0.0.0', // default: localhost
-    port: 8080 // default: 3000
+    host: '0.0.0.0' // default: localhost
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
