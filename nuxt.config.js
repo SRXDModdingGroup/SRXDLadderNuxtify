46d90343323d665a66f35073e8927e4e7f3ccfd8
Amy Y <ctrlalternativedelete@gmail.com>
2021-07-09 15:27:43 +1000

    changed name

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - SRXDLadderNuxtify',
+    titleTemplate: '%s - SRXDLadder',
     title: 'SRXDLadderNuxtify',
     meta: [
       { charset: 'utf-8' },
